5f98ef9e6079def6b373a2dca99a8b93f8df4bcf
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-06-01 12:44:00 +0000

    [add]association

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,2 +1,6 @@
 class Hobby < ApplicationRecord
+  belongs_to :user
+  belongs_to :tag
+  has_many :hobby_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
--- app/models/hobby_comment.rb
+++ app/models/hobby_comment.rb
@@ -1,2 +1,4 @@
 class HobbyComment < ApplicationRecord
+  belongs_to :hobby
+  belongs_to :user
 end
